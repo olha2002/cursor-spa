9d380928f23979254846663b5384a67e424f9441
Olha Orach <olhaorach2002@gmail.com>
2022-08-20 21:07:38 +0100

    Did one more change with navigation.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import "./styles/styles.css";
 import HomePage from "./components/Home/HomePage";
 import TweetsPage from "./components/Tweets/TweetsPage";
@@ -9,13 +9,13 @@ import { ContactsPage } from "./components/Contacts/ContactsPage";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Nav />
-        <Route path="/home" exact component={HomePage} />
+        <Route path="/" exact component={HomePage} />
         <Route path="/tweets" component={TweetsPage} />
         <Route path="/photos" component={PhotosPage} />
         <Route path="/contacts" component={ContactsPage} />
-      </Router>
+      </HashRouter>
     </div>
   );
 }
--- src/components/NavMenu/Nav.js
+++ src/components/NavMenu/Nav.js
@@ -8,7 +8,7 @@ export default class Nav extends Component {
       <nav className="header">
         <ul className="nav_links">
           <li className="nav_link">
-            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li className="nav_link">
             <NavLink to="/tweets">Twitters</NavLink>
